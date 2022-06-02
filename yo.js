4e3d961a4686b4836b74c297a4f0860f33884a5c
DrNykerstein09 <88530440+DrNykerstein09@users.noreply.github.com>
2022-06-02 13:58:30 -0500

    Update yo.js

--- yo.js
+++ yo.js
@@ -1,2 +1,2 @@
-const yo = ['Pierre', 16, false, '2006/05/24', 
-{ titulo: 'Ensayo sobre la ceguera', autor: 'JoseS', fecha: '2009/09/24', URL: 'https://es.wikipedia.org/wiki/Jos%C3%A9_Saramago'}]
+const yo = ['Pierre', 16, false, new Date(2006,05,24), 
+{ titulo: 'Ensayo sobre la ceguera', autor: 'JoseS', fecha: new Date(2009/09/24), URL: 'https://es.wikipedia.org/wiki/Jos%C3%A9_Saramago'}]
